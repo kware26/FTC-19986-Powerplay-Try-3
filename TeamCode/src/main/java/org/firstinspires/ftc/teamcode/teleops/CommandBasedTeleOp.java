d916a6a72a635adb10b5851978d6f011a8faf30c
kware26 <113393773+kware26@users.noreply.github.com>
2022-11-08 16:10:51 -0800

    New Tele-op Commands-driving, arm, and claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/CommandBasedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/CommandBasedTeleOp.java
@@ -5,9 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.BaseRobot;
 import org.firstinspires.ftc.teamcode.commands.DumpCargoCommand;
 import org.firstinspires.ftc.teamcode.commands.MoveArmCommand;
+import org.firstinspires.ftc.teamcode.commands.TestOpenClawCommand;
 import org.firstinspires.ftc.teamcode.shplib.commands.RunCommand;
 import org.firstinspires.ftc.teamcode.shplib.commands.Trigger;
 import org.firstinspires.ftc.teamcode.subsystems.ScoopSubsystem;
+
 
 @TeleOp
 public class CommandBasedTeleOp extends BaseRobot {
@@ -16,17 +18,20 @@ public class CommandBasedTeleOp extends BaseRobot {
     public void init() {
         super.init();
 
+
+
         // Default command runs when no other commands are scheduled for the subsystem
-//        drive.setDefaultCommand(
-//                new RunCommand(
-//                        () -> drive.mecanum(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x)
-//                )
-//        );
+        drive.setDefaultCommand(
+                new RunCommand(
+                        () -> drive.mecanum(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x)
+                )
+        );
     }
 
     @Override
     public void start() {
         super.start();
+        arm.slide.resetEncoder();
 
         // Add anything that needs to be run a single time when the OpMode starts
     }
@@ -34,10 +39,16 @@ public class CommandBasedTeleOp extends BaseRobot {
     @Override
     public void loop() {
         // Allows CommandScheduler.run() to be called - DO NOT DELETE!
-        super.loop();
 
-//        new Trigger(gamepad1.dpad_up, new MoveArmCommand(arm, MoveArmCommand.Direction.UP));
-//        new Trigger(gamepad1.dpad_down, new MoveArmCommand(arm, MoveArmCommand.Direction.DOWN));
+        super.loop();
+        //my code
+        new Trigger(gamepad1.dpad_left, new TestOpenClawCommand(claw, TestOpenClawCommand.Direction.CLOSED));
+        new Trigger(gamepad1.dpad_right, new TestOpenClawCommand(claw, TestOpenClawCommand.Direction.OPEN));
+//
+        new Trigger(gamepad1.dpad_up, new MoveArmCommand(arm, MoveArmCommand.Direction.TOP));
+//        new Trigger(gamepad1.dpad_down, new MoveArmCommand(arm, MoveArmCommand.Direction.BOTTOM));
+        //my code end
+
 //        new Trigger(gamepad1.b && !arm.atBottom(), new DumpCargoCommand(scoop));
 //
 //        // Dump cargo macro
@@ -52,10 +63,10 @@ public class CommandBasedTeleOp extends BaseRobot {
 //        );
 //
 //        intake.setPower(gamepad1.left_trigger - gamepad1.right_trigger);
-        if (gamepad1.a) {
-            claw.setPower(0.5);
-        } else if (gamepad1.b){
-            claw.setPower(-0.5);
-        } else claw.setPower(0.0);
+//        if (gamepad1.a) {
+//            claw.setPosition(0.5);
+//        } else if (gamepad1.b) {
+//            claw.setPosition(0.1);
+//        }
     }
 }
